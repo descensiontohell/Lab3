9908b13bd1dc7ee4b2eb7009208ff28542b1ed8b
descensiontohell <isuhanen@gmail.com>
2020-04-20 22:36:29 +0300

    third try

---
+++ Collab_goda/Collab_goda/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Collab_goda project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Collab_goda.settings')
+
+application = get_asgi_application()
